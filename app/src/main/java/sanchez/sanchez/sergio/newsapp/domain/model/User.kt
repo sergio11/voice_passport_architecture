8e5f366144c980de291ddb61395e23bcbc316de9
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-04-10 20:26:24 +0200

    Initial commit

--- app/src/main/java/sanchez/sanchez/sergio/newsapp/domain/model/User.kt
+++
@@ -1,9 +0,0 @@
-package sanchez.sanchez.sergio.newsapp.domain.model
-
-import android.net.Uri
-
-data class User (
-    var displayName: String,
-    var email: String,
-    var photoUrl: Uri?
-)
--- app/src/main/java/sanchez/sanchez/sergio/newsapp/ui/features/splash/SplashOperationResultEnum.kt
+++
@@ -1,7 +0,0 @@
-package sanchez.sanchez.sergio.newsapp.ui.features.splash
-
-
-enum class SplashOperationResultEnum {
-    USER_LOADED,
-    USER_NOT_AVAILABLE
-}
